d5c330970ca8643141100db94c522642cf7c83a1
maduwa416 <maduwa416@gmail.com>
2026-05-13 04:40:47 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,17 @@
+window.animeData = [
+    {
+        id: "solo-leveling",
+        name: "Solo Leveling",
+        season: "01",
+        releaseDate: "2024-01-01",
+        sinhalaSubs: [
+            { ep: 1, url: "link1.com" },
+            { ep: 2, url: "link2.com" }
+        ],
+        englishSubs: [
+            { ep: 1, url: "en-link1.com" }
+        ],
+        description: "Anime description here...",
+        time: new Date().getTime()
+    }
+];
