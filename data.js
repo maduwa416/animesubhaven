80c31e0ac34da39e0e7d24eb31a7d1a791af2a3a
animesubhaven <maduwa416@gmail.com>
2026-05-15 22:57:25 +0530

    Add files via upload

--- data.js
+++ data.js
@@ -1,31 +1,33 @@
-{
-    id, "wistoria:-wand-and-sword",
-    name, "Wistoria: Wand and Sword",
-    season, "02",
-    releaseDate, "2026 4 12",
-    description, "Wistoria: Wand and Sword follows Will Serfort, a hardworking student at a magical academy in a world where magic is absolute power. Despite being entirely unable to cast spells, Will uses his unmatched swordsmanship to battle dungeon monsters, defy elite peers, and chase a childhood promise to reach the top of the Wizard's Tower.",
-    sinhalaSubs, [
-        {
-                "ep": 1,
-                "url": "https://www.mediafire.com/file/c04eocxd3cl9ac4/wistoria.wand.and.sword.s2-e1ANIMESUBHAVEN.ass/file"
-        },
-        {
-                "ep": 2,
-                "url": "https://www.mediafire.com/file/0c1ax7cccapj01j/wistoria.wand.and.sword.s02e02ANIMESUBHAVEN.ass/file"
-        },
-        {
-                "ep": 3,
-                "url": "https://www.mediafire.com/file/08b0mab3ccy13ar/wistoria-wand-and-sword-s02-e03ANIMESUBHAVEN.ass/file"
-        },
-        {
-                "ep": 4,
-                "url": "https://www.mediafire.com/file/4okorr6kewfg4ly/wistoria-wand-and-sword-s2-e4ANIMESUBHAVEN.ass/file"
-        },
-        {
-                "ep": 5,
-                "url": "https://www.mediafire.com/file/sn5fxrklg9tj0is/wistoria.wand.and.sword.s02e05ANIMESUBHAVEN.ass/file"
-        }
-],
-    englishSubs, [],
-    time; 1778848335959
-};
+window.animeData = [
+    {
+        id: "wistoria-wand-and-sword",
+        name: "Wistoria: Wand and Sword",
+        season: "02",
+        releaseDate: "2026-04-12",
+        description: "Wistoria: Wand and Sword follows Will Serfort, a hardworking student at a magical academy in a world where magic is absolute power. Despite being entirely unable to cast spells, Will uses his unmatched swordsmanship to battle dungeon monsters, defy elite peers, and chase a childhood promise to reach the top of the Wizard's Tower.",
+        sinhalaSubs: [
+            {
+                ep: 1,
+                url: "https://www.mediafire.com/file/c04eocxd3cl9ac4/wistoria.wand.and.sword.s2-e1ANIMESUBHAVEN.ass/file"
+            },
+            {
+                ep: 2,
+                url: "https://www.mediafire.com/file/0c1ax7cccapj01j/wistoria.wand.and.sword.s02e02ANIMESUBHAVEN.ass/file"
+            },
+            {
+                ep: 3,
+                url: "https://www.mediafire.com/file/08b0mab3ccy13ar/wistoria-wand-and-sword-s02-e03ANIMESUBHAVEN.ass/file"
+            },
+            {
+                ep: 4,
+                url: "https://www.mediafire.com/file/4okorr6kewfg4ly/wistoria-wand-and-sword-s2-e4ANIMESUBHAVEN.ass/file"
+            },
+            {
+                ep: 5,
+                url: "https://www.mediafire.com/file/sn5fxrklg9tj0is/wistoria.wand.and.sword.s02e05ANIMESUBHAVEN.ass/file"
+            }
+        ],
+        englishSubs: [],
+        time: 1778848335959
+    }
+];
