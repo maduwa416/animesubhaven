affade846248c47b1df5bc0ec96c88ea27aa9a62
animesubhaven <maduwa416@gmail.com>
2026-05-15 18:06:57 +0530

    Add files via upload

--- data.js
+++ data.js
@@ -1,36 +1,31 @@
-window.animeData = [
-    {
-    id: "wistoria-wand-and-sword",
-    name: "wistoria wand and sword",
-    season: "02",
-    releaseDate: "2026 4 12",
-    imdb: "7.7",
-    genre: "fantasy action-adventure",
-    episodes: "??",
-    description: "Wistoria: Wand and Sword is a fantasy series set in a world where magical prowess defines a person's worth. It follows Will Serfort, a student who enrolls in a prestigious magic academy but cannot use magic. To keep a childhood promise, he relies on exceptional swordsmanship and physical strength to rise to the top",
-    sinhalaSubs: [
+{
+    id, "wistoria:-wand-and-sword",
+    name, "Wistoria: Wand and Sword",
+    season, "02",
+    releaseDate, "2026 4 12",
+    description, "Wistoria: Wand and Sword follows Will Serfort, a hardworking student at a magical academy in a world where magic is absolute power. Despite being entirely unable to cast spells, Will uses his unmatched swordsmanship to battle dungeon monsters, defy elite peers, and chase a childhood promise to reach the top of the Wizard's Tower.",
+    sinhalaSubs, [
         {
                 "ep": 1,
-                "url": "https://www.mediafire.com/file/pzybzqcvcphjxl8/Wistoria.Wand.And.Sword.S2_E1.zip/file"
+                "url": "https://www.mediafire.com/file/c04eocxd3cl9ac4/wistoria.wand.and.sword.s2-e1ANIMESUBHAVEN.ass/file"
         },
         {
                 "ep": 2,
-                "url": "https://www.mediafire.com/file/83on3cy78jn8epw/wistoria.wand.and.sword.s02e02.zip/file"
+                "url": "https://www.mediafire.com/file/0c1ax7cccapj01j/wistoria.wand.and.sword.s02e02ANIMESUBHAVEN.ass/file"
         },
         {
                 "ep": 3,
-                "url": "https://www.mediafire.com/file/8nyjys1qzjgnufn/Wistoria_Wand_And_Sword_S02_E03.zip/file"
+                "url": "https://www.mediafire.com/file/08b0mab3ccy13ar/wistoria-wand-and-sword-s02-e03ANIMESUBHAVEN.ass/file"
         },
         {
                 "ep": 4,
-                "url": "https://www.mediafire.com/file/zmm15y2pqzud5vl/wistoria-wand-and-sword-s2-e4.zip/file"
+                "url": "https://www.mediafire.com/file/4okorr6kewfg4ly/wistoria-wand-and-sword-s2-e4ANIMESUBHAVEN.ass/file"
         },
         {
                 "ep": 5,
-                "url": "https://www.mediafire.com/file/9jjdz7cbnjmqdkf/wistoria.wand.and.sword.s02e05.zip/file"
+                "url": "https://www.mediafire.com/file/sn5fxrklg9tj0is/wistoria.wand.and.sword.s02e05ANIMESUBHAVEN.ass/file"
         }
 ],
-    englishSubs: [],
-    time: 1778778197510
-},
-];
+    englishSubs, [],
+    time; 1778848335959
+};
