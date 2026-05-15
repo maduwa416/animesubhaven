84e690e1791e324b5af41c6117f4bc90f5d5bd73
animesubhaven <maduwa416@gmail.com>
2026-05-15 23:45:42 +0530

    Add files via upload

--- data.js
+++ data.js
@@ -1,7 +1,30 @@
 window.animeData = [
     {
-        id: "wistoria-wand-and-sword",
-        name: "Wistoria: Wand and Sword",
+    id: "wistoria-wand-and-sword-",
+    name: "Wistoria Wand and Sword S01",
+    season: "1",
+    releaseDate: "July 7, 2024",
+    description: "Wistoria: Wand and Sword follows Will Serfort, a hardworking student at a magical academy in a world where magic is absolute power. Despite being entirely unable to cast spells, Will uses his unmatched swordsmanship to battle dungeon monsters, defy elite peers, and chase a childhood promise to reach the top of the Wizard's Tower.",
+    sinhalaSubs: [
+        { ep: 1, url: "https://www.mediafire.com/file/n5isft3liu9pkfk/wistoria-ep-1.srt.zip/file" },
+        { ep: 2, url: "https://www.mediafire.com/file/aqcfuix3qay4d2y/wistoria-ep-2.srt.zip/file" },
+        { ep: 3, url: "https://www.mediafire.com/file/532z0qimrlzt0vu/wistoria-ep-3.srt.zip/file" },
+        { ep: 4, url: "https://www.mediafire.com/file/qq1mdxydh2rauv2/wistoria-ep-4.srt.zip/file" },
+        { ep: 5, url: "https://www.mediafire.com/file/6okz7orlveixsyh/wistoria-ep-5.srt.zip/file" },
+        { ep: 6, url: "https://www.mediafire.com/file/hqx58ovt5konmgv/wistoria-ep-6.srt.zip/file" },
+        { ep: 7, url: "https://www.mediafire.com/file/lpv9oxuyrknxuwb/Tsue-to-Tsurugi-no-Wistoria-07.zip/file" },
+        { ep: 8, url: "https://www.mediafire.com/file/jt0l7pb3utrsjtn/Tsue-to-Tsurugi-no-Wistoria-08.zip/file" },
+        { ep: 9, url: "https://www.mediafire.com/file/0ow0fnwkdp6295o/Tsue-to-Tsurugi-no-Wistoria-09.zip/file" },
+        { ep: 10, url: "https://www.mediafire.com/file/rbnfznxlpwcuo5l/wistoria-ep-10.srt.zip/file" },
+        { ep: 11, url: "https://www.mediafire.com/file/qj649bdxcpwhrwj/Tsue-to-Tsurugi-no-Wistoria-11.zip/file" },
+        { ep: 12, url: "https://www.mediafire.com/file/43awfam87092we1/Tsue-to-Tsurugi-no-Wistoria-12.zip/file" }
+    ],
+    englishSubs: [],
+    time: 1778868125523
+},
+    {
+        id: "wistoria-wand-and-sword-s02",
+        name: "Wistoria: Wand and Sword S02",
         season: "02",
         releaseDate: "2026-04-12",
         description: "Wistoria: Wand and Sword follows Will Serfort, a hardworking student at a magical academy in a world where magic is absolute power. Despite being entirely unable to cast spells, Will uses his unmatched swordsmanship to battle dungeon monsters, defy elite peers, and chase a childhood promise to reach the top of the Wizard's Tower.",
